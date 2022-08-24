5da556863ee82d047c2be4c5670365791333ac2c
Austin White <austinlldb@gmail.com>
2022-08-24 01:41:06 -0600

    structure

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- plugin management via packer
+require("plugins")
+
+-- vim mappings
+require("mappings")
+
+-- non plugin vim options
+require("options")
+
+-- auto commands
+require("autocmds")
